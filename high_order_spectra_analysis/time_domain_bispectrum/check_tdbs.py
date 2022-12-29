9f038ea97efc10927ab44216da84db6661d4a942
Matheus Anjos <matheusvra@hotmail.com>
2022-12-28 21:20:13 -0300

    feat (check_tdbs.py): removing breakpoint()

--- high_order_spectra_analysis/time_domain_bispectrum/check_tdbs.py
+++ high_order_spectra_analysis/time_domain_bispectrum/check_tdbs.py
@@ -5,22 +5,24 @@ from high_order_spectra_analysis.time_domain_bispectrum.tdbs import tdbs
 
 
 if __name__ == "__main__":
+
+    dtype = np.float32
     time_step = 0.003
     fs = 1/time_step
-    time = np.arange(0, 5, time_step, dtype=np.float32)
+    time = np.arange(0, 5, time_step, dtype=dtype)
 
-    freqs = np.array([11, 17, 21, 45], dtype=np.float32)
+    freqs = np.array([11, 17, 21, 45], dtype=dtype)
     w2, w4 = tuple(2*np.pi*np.array(freqs[1::2]))
-    phases =np.array([np.pi/20, np.pi/30, np.pi/6, np.pi/15], dtype=np.float32)
+    phases =np.array([np.pi/20, np.pi/30, np.pi/6, np.pi/15], dtype=dtype)
     phi2, phi4 = phases[1], phases[3]
-    gains = np.array([0.7, 1.15, 1.05, 0.93], dtype=np.float32)
+    gains = np.array([0.7, 1.15, 1.05, 0.93], dtype=dtype)
 
     clean_signal = np.cos((w2 + w4)*time + (phi2 + phi4))
 
     for freq, phase, gain in zip(freqs, phases, gains):
         clean_signal += gain*np.cos(2*np.pi*freq*time + phase)
 
-    signal = clean_signal.astype(np.float32)
+    signal = clean_signal.astype(dtype)
 
     frequency_array, spectrum, phase_spectrum, bispectrum, phase_bispectrum = tdbs(
         signal=signal,
@@ -30,9 +32,9 @@ if __name__ == "__main__":
         fmax=None,
         freq_step=0.05,
         phase_step=0.05,
-        dtype=np.float32
+        dtype=dtype
     )
-    breakpoint()
+
     max_freq_plot = 70
 
     fig = make_subplots(rows=3, cols=1)
