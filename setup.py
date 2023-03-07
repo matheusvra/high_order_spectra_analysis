69bc9f6d52dbdca6fb47a5adcbb66998720b5259
Matheus Anjos <matheusvra@hotmail.com>
2023-03-06 23:30:55 -0300

    feat(*): trying to making the external build work

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 
 setup(
     name='time_domain_trispectrum',
-    ext_modules=cythonize('tdts_cython.pyx'),
+    ext_modules=cythonize('high_order_spectra_analysis/time_domain_trispectrum/tdts_cython.pyx'),
     zip_safe=False,
     include_dirs=[numpy.get_include()]
 )
